dd4785dd92b0567a6ab21028ebdebcdec19fdbdb
ArnaudBvd <bravard.arnaud@gmail.com>
2025-01-20 18:01:04 +0100

    feat: ajout d'un script en JS pour générer un QR Code

---
+++ main.js
@@ -0,0 +1,26 @@
+const inputElement = document.querySelector('#generalInput');
+const generateButton = document.querySelector('#generate');
+const resultSection = document.querySelector('#result');
+const qrCodeCanvas = document.querySelector('#qrCode');
+const downloadLink = document.querySelector('#download');
+
+generateButton.addEventListener('click', ()=> {
+    const inputValue = inputElement.value.trim();
+    if(inputValue) {
+        QRCode.toCanvas(qrCodeCanvas, inputValue, {with:400}, (error) => {
+           if(error) {
+            console.error(error)
+            return
+           }
+           resultSection.classList.add('active');
+
+           const imageDataURL = qrCodeCanvas.toDataURL('image/png');
+           downloadLink.href= imageDataURL;
+           downloadLink.style.display='inline';
+
+        })
+    }else {
+        alert('Veuillez entrer un texte ou un lien pour générer un QR Code');
+    }
+})
+
